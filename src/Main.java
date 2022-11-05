216e435d3fce77613787dba86a179af667e58c15
q2q2q8 <art-kash@mail.ru>
2022-11-05 21:23:23 +0300

    шестое задание сделано

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         int contribution = 15000;
         int total = 0;
         int month = 0;
-        for (;total < 12_000_000;) {
+        for (;month <=108;) {
             contribution = contribution + contribution/100 * 7;
             total = contribution;
             month = month + 1;
