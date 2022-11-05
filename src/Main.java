48fea6bf33656bbaf1413a620ae2f167712d2823
q2q2q8 <art-kash@mail.ru>
2022-11-05 23:11:37 +0300

    восьмое задание сделано

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int contribution = 15000;
-        int total = 0;
-        int month = 0;
-        for (;month <=108;) {
-            contribution = contribution + contribution/100 * 7;
-            total = contribution;
-            month = month + 1;
-            if (month % 6 ==0) {
-                System.out.println("Месяц " + month + ", сумма накоплений " + total + " рублей");
-            }
-
+        int meteorite = 0;
+        for ( int year = 0; year <=2000; year++) {
+            meteorite = 79 + meteorite;
+            year = meteorite;
+            System.out.println(meteorite);
         }
-
     }
-    }
+}
