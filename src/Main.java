d552a56641afbd7078836bb078a2b00273a09228
q2q2q8 <art-kash@mail.ru>
2022-11-05 20:47:27 +0300

    третье задание сделано

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 0;
-        while (i < 10) {
-            i = i + 1;
-            System.out.print(i + " ");
+        int populationY = 12_000_000;
+        int yearFertility = 17 * (populationY / 1000);
+        int yearMortality = 8 * (populationY / 1000);
+        int year = 0;
+        while (year < 10) {
+            year = year + 1;
+            populationY = populationY + yearFertility - yearMortality;
+            System.out.println("Год " + year + " численность населения составляет " + populationY);
         }
-        System.out.println("");
-        for (i = 10; i >=1; i--) {
-        System.out.print(i + " ");
-        }
+
     }
     }
