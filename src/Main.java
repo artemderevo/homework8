1bfa85a561c98600e89ac1ff6243c47fa25e3277
q2q2q8 <art-kash@mail.ru>
2022-11-07 00:01:33 +0300

    девятое задание сделано

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int meteorite = 0;
-        for ( int year = 0; year <=2000; year++) {
+        for ( int year = 0; year <=2050; year++) {
             meteorite = 79 + meteorite;
             year = meteorite;
-            System.out.println(meteorite);
+            if (meteorite > 1850 && meteorite < 2100)
+        System.out.println(meteorite);
         }
     }
 }
